028189561a4d8826acd4ce7dc6dff4a587eaea6a
Henry Seeger <hiseeger@gmail.com>
2026-06-27 20:49:58 -0400

    Parameterized everything, n x n vertical button alignments allowed

--- radio_buttons.py
+++ radio_buttons.py
@@ -3,43 +3,23 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-import matplotlib.pyplot as plt
 
-if "rad1" not in st.session_state:
-    st.session_state.rad1 = 1
+rad_options = [1, "2", 3, "giraffe", 3.14]
 
-if "rad2" not in st.session_state:
-    st.session_state.rad2 = 2
+for i, default_val in enumerate(rad_options):
+  key = f"rad{i}"
+  if key not in st.session_state:
+    st.session_state[key] = rad_options[i]
 
-if "rad3" not in st.session_state:
-    st.session_state.rad3 = 3
-
-cols = st.columns([1, 1, 1], border = True)
-
-rad_options = ["1", 2, 3, 3.5, "giraffe"]
+cols = st.columns(len(rad_options), border = False)
 
 def rad_change(changed):
-    index_sum = len(rad_options) * (len(rad_options) - 1) / 2 # -1 instead of +1 because of zero-indexing
-    r1 = st.session_state.rad1
-    r2 = st.session_state.rad2
-    r3 = st.session_state.rad3
-    
-    if changed == 1:
-        if r1 == r2:
-            st.session_state.rad2 = rad_options[int(index_sum - rad_options.index(r1) - rad_options.index(r3))]
-        elif r1 == r3:
-            st.session_state.rad3 = rad_options[int(index_sum - rad_options.index(r1) - rad_options.index(r2))]
-    if changed == 2:
-        if r2 == r1:
-            st.session_state.rad1 = rad_options[int(index_sum - rad_options.index(r3) - rad_options.index(r2))]
-        elif r2 == r3:
-            st.session_state.rad3 = rad_options[int(index_sum - rad_options.index(r1) - rad_options.index(r2))]
-    if changed == 3:
-        if r3 == r1:
-            st.session_state.rad1 = rad_options[int(index_sum - rad_options.index(r3) - rad_options.index(r2))]
-        elif r3 == r2:
-            st.session_state.rad2 = rad_options[int(index_sum - rad_options.index(r3) - rad_options.index(r1))]
+  index_sum = len(rad_options) * (len(rad_options) - 1) / 2 # -1 instead of +1 because of zero-indexing
+  for i in [i for i in range(len(cols[:])) if i != changed]:
+    if st.session_state[f"rad{changed}"] == st.session_state[f"rad{i}"]:
+      st.session_state[f"rad{i}"] = rad_options[int(index_sum - sum([rad_options.index(st.session_state[f"rad{j}"]) for j in range(len(cols)) if j != i]))]
+      break
 
 for i, col in enumerate(cols):
-    with col:
-        st.radio(label = f"rad{i + 1}", options = rad_options, key = f"rad{i + 1}", on_change = rad_change, args = (i + 1,))
+  with col:
+    st.radio(label = f"rad{i}", options = rad_options, horizontal = False, key = f"rad{i}", on_change = rad_change, args = (i,))
