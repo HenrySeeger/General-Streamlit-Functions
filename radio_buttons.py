1b9f82ed7a48a956d9fa020925e002fd9072ae45
Henry Seeger <hiseeger@gmail.com>
2026-06-29 10:11:36 -0400

    Allowed for radio buttons to be aligned horizonatlly, meaning num rows can be greater than, equal to, or less than num columns. Some text formatting still required.

--- radio_buttons.py
+++ radio_buttons.py
@@ -8,21 +8,27 @@ rad_options = [1, "2", 3, "giraffe", 3.14, 6, 7, 8]
 
 num_cols = 4
 num_cols = min(num_cols, len(rad_options))
+var_names = [f"var{i}" for i in range(num_cols)]
+
+horizontal = True
 
 for i, default_val in enumerate(rad_options):
   key = f"rad{i}"
   if key not in st.session_state:
     st.session_state[key] = rad_options[i]
 
-cols = st.columns(num_cols, border = True)
+cols = list(range(num_cols)) if horizontal else st.columns(num_cols, border = False)
 
 def rad_change(changed):
-  for i in [i for i in range(len(cols[:])) if i != changed]:
+  for i in [i for i in range(num_cols) if i != changed]:
     if st.session_state[f"rad{changed}"] == st.session_state[f"rad{i}"]:
-      available_indices = list(set(range(len(rad_options))) ^ {rad_options.index(st.session_state[f"rad{j}"]) for j in range(len(cols)) if j != i})
+      available_indices = list(set(range(len(rad_options))) ^ {rad_options.index(st.session_state[f"rad{j}"]) for j in range(num_cols) if j != i})
       st.session_state[f"rad{i}"] = rad_options[available_indices[0]]
       break
 
 for i, col in enumerate(cols):
-  with col:
-    st.radio(label = f"rad{i}", options = rad_options, horizontal = False, key = f"rad{i}", on_change = rad_change, args = (i,))
+  try:
+    with col:
+      st.radio(label = var_names[i], options = rad_options, horizontal = horizontal, key = f"rad{i}", on_change = rad_change, args = (i,))
+  except:
+    st.radio(label = var_names[i], options = rad_options, horizontal = horizontal, key = f"rad{i}", on_change = rad_change, args = (i,))
